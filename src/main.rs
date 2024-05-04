f5bfa811a20a5bb3dda0fa0253c3fb8487f4e0c7
InimaJin <>
2024-05-04 11:30:04 +0200

    Game works as expected.

--- src/main.rs
+++ src/main.rs
@@ -4,31 +4,61 @@ use std::io::{self, Read};
 fn main() {
     //The indices within field where 'X' or 'O' can be placed
     let mut indices: Vec<usize> = Vec::with_capacity(9);
-    let field_vec = create_field("field.txt", &mut indices)
-        .unwrap_or_else(|error|{ 
-            panic!("Failed to open field file:\n{}", error);
-        });
-   
+    // The vector holding the game's field. Instead of the 'r' placeholder for the squares where players can insert, it has whitespaces.
+    let mut field_vec = create_field("field.txt", &mut indices).unwrap_or_else(|error| {
+        panic!("Couldn't open field file:\n{}", error);
+    });
     
-    let player1: Vec<i32> = Vec::with_capacity(5);
-    let player1_turn = true;
-    let player2: Vec<i32> = Vec::with_capacity(5);
-    
-    loop {
+    // These two vectors hold true at the indices where players inserted their symbol, false for
+    // the rest
+    let mut player1: Vec<bool> = vec![false; 9];
+    let mut player2: Vec<bool> = vec![false; 9];
+    let mut player1_turn = true;
+    let mut winner = 0;
+
+    let mut msg = String::new();
+    while winner == 0 {
+        draw_game(&field_vec, &mut msg, true);
+
         let mut player_input = String::new();
         io::stdin()
             .read_line(&mut player_input)
             .expect("Readline operation failed");
-        let location = PlayerMove::new(player_input.trim());
-        let location = match location {
-            Ok(val) => val,
+        if player_input.len() < 2 {
+            println!("Please specify two coordinates. E.g. B2");
+            continue;
+        }
+
+        let player_move = PlayerMove::new(player_input.trim());
+        let player_move = match player_move {
+            Ok(instance) => instance,
             Err(e) => {
-                println!("{}", e);
+                msg = e;
                 continue;
             }
         };
-        println!("{}", location.square_num);
+        if player1[player_move.square_val] || player2[player_move.square_val] {
+            msg = String::from("This field is already occupied. Choose another one.");
+            continue;
+        }
+        if player1_turn {
+            player1[player_move.square_val] = true;
+            field_vec[*indices.get(player_move.square_val).unwrap()] = 'X';
+            if tic_tac_toe(&player1) {
+                winner = 1;
+            }
+        } else {
+            player2[player_move.square_val] = true;
+            field_vec[*indices.get(player_move.square_val).unwrap()] = 'O';
+            if tic_tac_toe(&player2) {
+                winner = 2;
+            }
+        }
+        player1_turn = !player1_turn;
+
     }
+    draw_game(&field_vec, &mut msg, false);
+    println!("\u{001b}[35mCongratulations, player {}. You win!\u{001b}[0m", winner);
 }
 
 // Reads a tic-tac-toe field template from a file and feeds each of its characters into the field_vec
@@ -36,11 +66,11 @@ fn main() {
 // with a whitespace and pushing the index of that whitespace into the indices vector
 fn create_field(filename: &str, indices: &mut Vec<usize>) -> Result<Vec<char>, io::Error> {
     let mut file = File::open(filename)?;
-    
+
     let mut field_string = String::new();
     file.read_to_string(&mut field_string);
 
-    let mut field_vec: Vec<char> = Vec::with_capacity(field_string.len()); 
+    let mut field_vec: Vec<char> = Vec::with_capacity(field_string.len());
     for (i, c) in field_string.chars().enumerate() {
         if c == 'r' {
             indices.push(i);
@@ -49,38 +79,57 @@ fn create_field(filename: &str, indices: &mut Vec<usize>) -> Result<Vec<char>, i
         }
         field_vec.push(c);
     }
+    if indices.len() != 9 {
+        panic!("Invalid tic-tac-toe field. Should have excatly 9 insert positions specified");
+    }
     Ok(field_vec)
 }
 
-#[derive(Debug)]
+// Draws the field in its current state after clearing the screen, prints the prompt to enter
+// coordinates and prints the message possibly containing an error
+fn draw_game(field_vec: &Vec<char>, msg: &mut String, prompt_user: bool) {
+    let field_as_string: String = field_vec.iter().collect();
+    println!("{esc}[2J{esc}[1;1H", esc = 27 as char);
+    println!("{}", field_as_string);
+    if prompt_user { println!("{}\nEnter coordinate:", msg) }
+    msg.clear();
+}
+
+// Represents a move the player has made
 struct PlayerMove {
     coord_letter: char,
     coord_digit: i32,
-    square_num: i32
+    square_val: usize,
 }
 
 impl PlayerMove {
     // Creates a new instance of PlayerMove from a coordinates input (e.g. a2, c3, B1 etc...)
     // Calls compute_square_num() before returning the finished instance of PlayerMove.
-    fn new(coord: &str) -> Result<PlayerMove, String> {
-        let mut point = Self { coord_letter: ' ', coord_digit: 0, square_num: 0 };
+    fn new(coord: &str) -> Result<Self, String> {
+        let mut point = Self {
+            coord_letter: ' ',
+            coord_digit: 0,
+            square_val: 0,
+        };
         let mut digit: i32;
         for c in coord.chars() {
             if c != ' ' {
                 if c.is_digit(10) && point.coord_digit == 0 {
-                    digit = String::from(c).parse().unwrap();         
+                    digit = String::from(c).parse().unwrap();
                     if digit > 3 {
-                        return Err(format!("Invalid input: {} is out of bounds.\nShould be between 1 and 3.", digit));
+                        return Err(format!(
+                            "Invalid column: {} is out of bounds.\nShould be between 1 and 3.",
+                            digit
+                        ));
                     }
                     point.coord_digit = digit;
-                }
-                else {
-                    if point.coord_letter == ' ' { 
+                } else if c.is_alphabetic() {
+                    if point.coord_letter == ' ' {
                         point.coord_letter = match c {
                             'a' => c,
                             'b' => c,
                             'c' => c,
-                            _ => return Err(format!("Invalid input: {}", c))
+                            _ => return Err(format!("Invalid row: '{}' - Should be a, b or c.", c)),
                         }
                     }
                 }
@@ -89,18 +138,35 @@ impl PlayerMove {
                 break;
             }
         }
-        point.square_num = point.compute_square_num()?;
+        point.compute_square_val()?;
         Ok(point)
     }
     // Computes the number of the field square that this PlayerMove instance was placed in
-    // Example: Field at coordinates a1 is number 1. a3 is number 3. b2 is number 5 etc...  
-    fn compute_square_num(&mut self) -> Result<i32, String> {
-        let num = match self.coord_letter {         
-            'a' => self.coord_digit,
-            'b' => self.coord_digit + 3,
-            'c' => self.coord_digit + 6,
-            _ => panic!("error")
+    // Example: Field at coordinates a1 is number 0. a3 is number 2. b2 is number 4 etc...
+    // Sets the square_val value to that number
+    fn compute_square_val(&mut self) -> Result<usize, String>  {
+        let val = match self.coord_letter {
+            'a' => self.coord_digit - 1,
+            'b' => self.coord_digit + 2,
+            'c' => self.coord_digit + 5,
+            _ => return Err("Invalid letter.".to_string()),
         };
-        Ok(num)
+        self.square_val = val as usize;
+        Ok(self.square_val)
     }
+}
+
+// Checks if the current player's vector has three in a row by
+// examining all combinations for three 'true' values and returning true
+// if one matches
+fn tic_tac_toe(player_vec: &Vec<bool>) -> bool {
+    return &player_vec[0..3] == &[true, true, true]
+        || &player_vec[3..6] == &[true, true, true]
+        || &player_vec[6..9] == &[true, true, true]
+        || player_vec[0] && player_vec[3] && player_vec[6]
+        || player_vec[1] && player_vec[4] && player_vec[7]
+        || player_vec[2] && player_vec[5] && player_vec[8]
+        || player_vec[0] && player_vec[4] && player_vec[8]
+        || player_vec[2] && player_vec[4] && player_vec[6];
+    
 }
